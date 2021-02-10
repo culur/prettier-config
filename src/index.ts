d096b6371bbdf93032d8f804b35b3497187e07db
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2021-02-10 18:34:01 +0700

    feat(prettier): add config

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { PrettierOptions } from './typings/prettier';
+import os from 'os';
+
+const isWin = os.platform() === 'win32';
+const options: PrettierOptions = {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    htmlWhitespaceSensitivity: 'ignore',
+    printWidth: 80,
+    endOfLine: isWin ? 'crlf' : 'lf',
+    overrides: [
+        {
+            files: [
+                // alphabetical order
+                '.babelrc',
+                '.czrc',
+                '.eslintrc',
+                '.prettierrc',
+                '.releaserc',
+                '.stylelintrc',
+                '*.code-workspace',
+            ],
+            options: { parser: 'json' },
+        },
+        {
+            files: [
+                // alphabetical order
+                'package.json',
+                'package-lock.json',
+                '*.md',
+                '*.yml',
+            ],
+            options: { tabWidth: 2 },
+        },
+    ],
+};
+
+export = options;
